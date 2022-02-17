48b7a00342ee82389a3ac31f35e829acc5a65114
NatanSL <ntsilvalima@gmail.com>
2022-02-16 22:43:25 -0300

    Finalização projeto

--- app.js
+++ app.js
@@ -20,8 +20,10 @@ app.get('/', async (req,res) => {
 
    
     //Consult API   
-    const data = await axios(`https://cosmoserp.com/zhaz/aWSPCosmosFBX.aspx?${key},vApiOS,3`);    
+    const data = await axios(`https://cosmoserp.com/zhaz/aWSPCosmosFBX.aspx?${key},vApiOS,2`);    
     const suporte = data.data;  
+    await formataData(suporte);
+    await formataHora(suporte);
     let dados = {dado: []};
 
     for(var i = 0; i < suporte.length; i++) {
@@ -43,7 +45,7 @@ app.get('/', async (req,res) => {
     const active = "active";
     const desactive = "";
 
-    res.render('emAbertos', {active, desactive})
+    res.render('emAbertos', {active, desactive, dados})
     console.log(dados.dado)
     
     
@@ -53,8 +55,11 @@ app.get('/', async (req,res) => {
 app.get('/finalizados', async (req,res)  =>{
 
     //Consult API
-    const data2 = await axios(`https://cosmoserp.com/zhaz/aWSPCosmosFBX.aspx?${key},vApiOS,14`);
+    const data2 = await axios(`https://cosmoserp.com/zhaz/aWSPCosmosFBX.aspx?${key},vApiOS,15`);
     const finalizados = data2.data;
+    console.log(finalizados);
+    await formataData(finalizados);
+
     let dadosFinalizados = {dado: []};
 
     for (var i = 0; i < finalizados.length; i++){
@@ -66,15 +71,18 @@ app.get('/finalizados', async (req,res)  =>{
             Cliente: finalizados[i].PessoaFantasia,
             Vendedor: finalizados[i].AgenteNegNome,
             Equipamento: finalizados[i].NomeEquipamento,
-            NS: finalizados[i].EquipamentoLTS
+            NS: finalizados[i].EquipamentoLTS,
+            Data_Movimento: finalizados[i].DataFinalMovto
            });
         }         
     }
+
+    console.log(dadosFinalizados)
     // res.json(dadosFinalizados.dado);
     const active = "desactive";
     const desactive = "active";
     
-    res.render('finalizados', {active, desactive});
+    res.render('finalizados', {active, desactive, dadosFinalizados});
 });
 
 
@@ -93,7 +101,28 @@ app.listen(port, (err) => {
 })
 
 
+//Functions
 
+formataData = async (dataItem) => {
+    for (var i = 0; i < dataItem.length; i++) {
+        var dataA = dataItem[i];
+        var dataF = await dataA.OSData.replace(/(\d*)-(\d*)-(\d*)T(\d*):(\d*).*/, '$3/$2/$1-$4:$5');
+        dataA.OSData = await dataF;
+    }
+
+    for (var i = 0; i < dataItem.length; i++) {
+        var dataA = dataItem[i];
+        var dataF = await dataA.DataFinalMovto.replace(/(\d*)-(\d*)-(\d*)T(\d*):(\d*).*/, '$3/$2/$1 $4:$5');
+        dataA.DataFinalMovto = await dataF;
+    }
+}
+
+formataHora = async (dataHora) =>{
+    for (var i = 0; i < dataHora.length; i++) {
+        var data = await dataHora[i];
+        var hora = await data.DataFinalMovto.replace(/(\d*)/, )  
+    }
+}
 
 
 // https://cosmoserp.com/zhaz/aWSPCosmosFBX.aspx?f2117e5dfa7f998f93afd92547d0ba9b,vApiOS,4
